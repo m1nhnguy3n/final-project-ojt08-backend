687288812a0989bc5f632e39773d5369eabbfd72
m1nhnguy3n <minhdacamst@gmail.com>
2024-04-16 09:32:31 +0700

    fix adminkey

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./data/firestoreAdminKeyDemo.json');
+const serviceAccount = require('./data/firestoreAdminKey.json');
 
 // admin.initializeApp({
 //     credential: admin.credential.cert(serviceAccount),
